03ec8b4bc4eae6110a071006286ac3460bcc5ced
Ashish Muralidharan <ashish.muralidharan@freshworks.com>
2019-03-10 13:42:51 +0530

    testing django URLS

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def welcome(request):
+    return render(request, "welcome.html")
+
--- getSQL/settings.py
+++ getSQL/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'getSQL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
