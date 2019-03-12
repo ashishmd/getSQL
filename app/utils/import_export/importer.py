90a9cce09820cfa4ef23b052307f6bf96033d6aa
Ashish Muralidharan <ashish.muralidharan@freshworks.com>
2019-03-12 21:07:17 +0530

    added comments

--- app/utils/import_export/importer.py
+++ app/utils/import_export/importer.py
@@ -5,9 +5,9 @@ from app.models import Tables
 FILE_PATH = "test_files/test.csv"
 
 
+# @todo : currently we are importing test file. Later we will implement upload option.
 def import_tables():
     table_names = read_csv.read_table_names(FILE_PATH)
-
     for table in table_names:
         table_data = Tables(table_name=table["Table Name"], alias=table["Alias"])
         table_data.save()
--- app/views.py
+++ app/views.py
@@ -5,16 +5,18 @@ from mysql.connector import errorcode
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Welcome to getSQl")
 
 
 def welcome(request):
     return render(request, "welcome.html")
 
 
+# Below method will check if mysql connection is working fine or not.
+# It will check for db, and if found, will return success message.
 def connection(request):
     try:
-        cnx = mysql.connector.connect(user='root', database='freshsales_dev')
+        cnx = mysql.connector.connect(user='root', database='get_sql')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             return HttpResponse("Something is wrong with your user name or password")
@@ -27,11 +29,14 @@ def connection(request):
         return HttpResponse("Connection Success. Database Exist")
 
 
+# Below method will import data to table app_tables
 def migrate_tables(request):
     from app.utils.import_export import importer
     return HttpResponse(importer.import_tables())
 
 
+# below method will be used to delete all tables in the current DB.
+# run migrate task after doing below operation
 def reinit_db(request):
     from app.utils.sql import reinit_db
     if reinit_db.delete_all_tables():
