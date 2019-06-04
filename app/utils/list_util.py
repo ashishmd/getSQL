f57f88e4fa33e99ef51e85a898edf353acf11d2e
ashishmd <muralidharanashish@gmail.com>
2019-06-05 00:56:36 +0530

    Refractored code (#9)

--- app/utils/list_util.py
+++ app/utils/list_util.py
@@ -3,3 +3,8 @@ def get_values_from_query_set(query_set, key):
     for row in query_set.iterator():
         result.append(getattr(row, key))
     return result
+
+
+def append_to_list_with_check(value, data_list):
+    if value and value not in data_list:
+        data_list.append(value)
